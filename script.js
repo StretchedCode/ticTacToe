a84bb83d48c914e9efab3e2a33f87930cca52363
Ghufran <ghufranshahid101@gmail.com>
2022-12-29 21:23:56 -0500

    Added Testing for DOM elements to be appended to gameBoardContainer

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const gameBoard = (() => {
     for (let i = 0; i < 9; i += 1) {
       const tile = document.createElement('div');
       tile.setAttribute('data-index', i);
+      tile.innerText = 'Test';
+      tiles.push(tile);
     }
+    tiles.forEach((tile) => {
+      boardContainer.append(tile);
+    });
   }
+
+  return { createTiles };
 })();
+
+gameBoard.createTiles();
