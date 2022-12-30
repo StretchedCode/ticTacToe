da7f9af4155af6692f7a8c7ad7dfb9719034c7f7
Ghufran <ghufranshahid101@gmail.com>
2022-12-29 22:43:08 -0500

    Removed placeholder content from tile containers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameController = (() => {
   let turns = 0;
   function takeTurn(tile) {
-    if (turns === 0) { tile.innerText = 'X'; } else {
+    if (turns % 2 === 0) { tile.innerText = 'X'; } else {
       tile.innerText = 'O';
       console.log('test');
     }
@@ -19,7 +19,6 @@ const gameBoard = ((turnFunction) => {
     for (let i = 0; i < 9; i += 1) {
       const tile = document.createElement('button');
       tile.setAttribute('data-index', i);
-      tile.innerText = 'X';
       tile.addEventListener('click', () => { turnFunction(tile); });
       tiles.push(tile);
     }
