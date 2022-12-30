688f72217eb1fe7f71b72a829f73d4a46e85d0dd
Ghufran <ghufranshahid101@gmail.com>
2022-12-29 22:40:56 -0500

    Added function to change tile contents, restructured file for consistent declaration

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const gameBoard = (() => {
+const gameController = (() => {
+  let turns = 0;
+  function takeTurn(tile) {
+    if (turns === 0) { tile.innerText = 'X'; } else {
+      tile.innerText = 'O';
+      console.log('test');
+    }
+    turns += 1;
+  }
+
+  return { takeTurn };
+})();
+
+const gameBoard = ((turnFunction) => {
   const tiles = [];
   const boardContainer = document.querySelector('.gameBoardContainer');
 
@@ -7,6 +20,7 @@ const gameBoard = (() => {
       const tile = document.createElement('button');
       tile.setAttribute('data-index', i);
       tile.innerText = 'X';
+      tile.addEventListener('click', () => { turnFunction(tile); });
       tiles.push(tile);
     }
     tiles.forEach((tile) => {
@@ -15,24 +29,6 @@ const gameBoard = (() => {
   }
 
   return { createTiles, tiles };
-})();
-
-const displayController = ((board) => {
-  function test() {
-    console.log('placeholder');
-  }
-})(gameBoard.tiles);
-
-const gameController = ((board, player) => {
-  const turns = 0;
-
-  board.forEach((tile) => {
-    tile.addEventListener('click');
-  });
-
-  function takeTurn() {
-
-  }
-})(gameBoard.tiles);
+})(gameController.takeTurn);
 
 gameBoard.createTiles();
