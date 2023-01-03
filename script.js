672d535d14061cc767c4353cb83a096094c2a2b1
Ghufran <ghufranshahid101@gmail.com>
2023-01-02 20:50:41 -0500

    Turn function now passes in tile array, cleaned createTiles function and made sure win conditions worked

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameController = (() => {
   let turns = 0;
-  function takeTurn(tile) {
+  function takeTurn(tile, tiles) {
     if (tile.getAttribute('used') === 'true') {} else {
       if (turns % 2 === 0) { tile.innerText = 'X'; } else {
         tile.innerText = 'O';
@@ -8,10 +8,12 @@ const gameController = (() => {
 
       tile.setAttribute('used', true);
       turns += 1;
+
+      checkGame(tiles, turns);
     }
   }
 
-  function checkGame(tiles) {
+  function checkGame(tiles, turnCount) {
     // This function checks for win conditions throughout the gameboard, and calls a tie if needed.
 
     function circleWinConditions() {
@@ -26,13 +28,16 @@ const gameController = (() => {
 
     function crossWinConditions() {
       if ((tiles[0].innerText === 'X' && tiles[4].innerText === 'X' && tiles[8].innerText === 'X') || (tiles[2].innerText === 'X' && tiles[4].innerText === 'X' && tiles[6].innerText === 'X')) {
-        console.log('circle wins!');
+        console.log('cross wins!');
       } else if ((tiles[0].innerText === 'X' && tiles[1].innerText === 'X' && tiles[2].innerText === 'X') || (tiles[3].innerText === 'X' && tiles[4].innerText === 'X' && tiles[5].innerText === 'X') || (tiles[6].innerText === 'X' && tiles[7].innerText === 'X' && tiles[8].innerText === 'X')) {
-        console.log('circle wins!');
+        console.log('cross wins!');
       } else if ((tiles[0].innerText === 'X' && tiles[3].innerText === 'X' && tiles[6].innerText === 'X') || (tiles[1].innerText === 'X' && tiles[4].innerText === 'X' && tiles[7].innerText === 'X') || (tiles[2].innerText === 'X' && tiles[5].innerText === 'X' && tiles[8].innerText === 'X')) {
-        console.log('circle wins!');
+        console.log('cross wins!');
       }
     }
+
+    circleWinConditions();
+    crossWinConditions();
   }
 
   return { takeTurn };
@@ -45,10 +50,10 @@ const gameBoard = ((turnFunction) => {
   function createTiles() {
     for (let i = 0; i < 9; i += 1) {
       const tile = document.createElement('button');
-      tile.setAttribute('data-index', i);
-      tile.setAttribute('used', false);
-      tile.addEventListener('click', () => { turnFunction(tile); });
       tiles.push(tile);
+      tiles[i].setAttribute('data-index', i);
+      tiles[i].setAttribute('used', false);
+      tiles[i].addEventListener('click', () => { turnFunction(tiles[i], tiles); });
     }
     tiles.forEach((tile) => {
       boardContainer.append(tile);
